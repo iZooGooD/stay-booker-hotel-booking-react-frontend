acbfb8811d4539d48ea7d9f5c4ab7478acb5706b
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-01-18 08:54:50 +0530

    Add mock data

--- src/mirage/mirageServer.js
+++ src/mirage/mirageServer.js
@@ -96,6 +96,73 @@ export function makeServer({ environment = 'development' } = {}) {
                   price: '18,900',
                   ratings: '8.8',
                 },
+                {
+                  hotelCode: 71223,
+                  image: {
+                    imageUrl: '/images/hotels/465660377/465660377.jpg',
+                    accessibleText: 'Courtyard by Marriott Pune',
+                  },
+                  title: 'Courtyard by Marriott Pune Hinjewadi',
+                  subtitle: '500 meters from the Rajiv Gandhi Infotech Park',
+                  benefits: [
+                    'Free cancellation',
+                    'No prepayment needed – pay at the property',
+                    'Free wifi',
+                    'Free lunch',
+                  ],
+                  price: '25,300',
+                  ratings: '8.8',
+                },
+                {
+                  hotelCode: 71224,
+                  image: {
+                    imageUrl: '/images/hotels/469186143/469186143.jpg',
+                    accessibleText: 'The Westin Pune Koregaon Park',
+                  },
+                  title: 'The Westin Pune Koregaon Park',
+                  subtitle: '5.4 km from centre',
+                  benefits: [
+                    'Free cancellation',
+                    'No prepayment needed – pay at the property',
+                    'Free wifi',
+                  ],
+                  price: '11,300',
+                  ratings: '5.8',
+                },
+                {
+                  hotelCode: 71225,
+                  image: {
+                    imageUrl: '/images/hotels/252004905/252004905.jpg',
+                    accessibleText: 'Novotel Pune Viman Nagar Road',
+                  },
+                  title: 'Novotel Pune Viman Nagar Road',
+                  subtitle: 'Weikfield IT City Infopark | 7.1 km from centre',
+                  benefits: [
+                    'Pets allowed',
+                    'Dinner + Lunch included',
+                    'Free wifi',
+                    'Free taxi from airport',
+                  ],
+                  price: '14,599',
+                  ratings: '7.8',
+                },
+                {
+                  hotelCode: 71226,
+                  image: {
+                    imageUrl: '/images/hotels/54360345/54360345.jpg',
+                    accessibleText: 'Vivanta Pune',
+                  },
+                  title: 'Vivanta Pune',
+                  subtitle: 'Xion Complex, | 14.2 km from centre',
+                  benefits: [
+                    'Pets allowed',
+                    'Free wifi',
+                    'Free cancellation',
+                    'No prepayment needed – pay at the property',
+                  ],
+                  price: '9,799',
+                  ratings: '9.3',
+                },
               ],
             },
           }
