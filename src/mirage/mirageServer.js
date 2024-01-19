39ee6eba740533ae86bbd6da4a06c3807e4312a1
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-01-19 08:59:42 +0530

    mock new endpoint

--- src/mirage/mirageServer.js
+++ src/mirage/mirageServer.js
@@ -95,6 +95,7 @@ export function makeServer({ environment = 'development' } = {}) {
                   ],
                   price: '18,900',
                   ratings: '8.8',
+                  city: 'pune',
                 },
                 {
                   hotelCode: 71223,
@@ -112,6 +113,7 @@ export function makeServer({ environment = 'development' } = {}) {
                   ],
                   price: '25,300',
                   ratings: '8.8',
+                  city: 'pune',
                 },
                 {
                   hotelCode: 71224,
@@ -128,6 +130,7 @@ export function makeServer({ environment = 'development' } = {}) {
                   ],
                   price: '11,300',
                   ratings: '5.8',
+                  city: 'pune',
                 },
                 {
                   hotelCode: 71225,
@@ -145,6 +148,7 @@ export function makeServer({ environment = 'development' } = {}) {
                   ],
                   price: '14,599',
                   ratings: '7.8',
+                  city: 'pune',
                 },
                 {
                   hotelCode: 71226,
@@ -162,11 +166,137 @@ export function makeServer({ environment = 'development' } = {}) {
                   ],
                   price: '9,799',
                   ratings: '9.3',
+                  city: 'pune',
                 },
               ],
             },
           }
         );
+      });
+
+      this.get('/hotests', (request) => {
+        if (request.queryParams.city === 'mumbai') {
+          return new Response(
+            200,
+            {},
+            {
+              errors: [],
+              data: {
+                elements: [
+                  {
+                    path: '/nearbyHotels',
+                    response: {
+                      statusCode: 200,
+                      headers: {},
+                      body: {
+                        errors: [],
+                        data: {
+                          elements: [
+                            {
+                              hotelCode: 81331,
+                              image: {
+                                imageUrl:
+                                  '/images/hotels/348592039/348592039.jpg',
+                                accessibleText: 'Taj Mahal Palace Mumbai',
+                              },
+                              title: 'The Taj Mahal Palace, Mumbai',
+                              subtitle:
+                                'Apollo Bunder, Mumbai | 2.5 kms from city center',
+                              benefits: [
+                                'Free cancellation',
+                                'Pay later option available',
+                                'Free breakfast',
+                              ],
+                              price: '35,000',
+                              ratings: '9.2',
+                              city: 'mumbai',
+                            },
+                            {
+                              hotelCode: 81332,
+                              image: {
+                                imageUrl:
+                                  '/images/hotels/500213112/500213112.jpg',
+                                accessibleText: 'Oberoi Mumbai',
+                              },
+                              title: 'The Oberoi Mumbai',
+                              subtitle:
+                                'Nariman Point, Mumbai | 3 kms from city center',
+                              benefits: [
+                                'Ocean view rooms',
+                                'Free wifi',
+                                'Spa services',
+                              ],
+                              price: '30,750',
+                              ratings: '9.5',
+                              city: 'mumbai',
+                            },
+                            {
+                              hotelCode: 81333,
+                              image: {
+                                imageUrl:
+                                  '/images/hotels/114422003/114422003.jpg',
+                                accessibleText: 'Trident Bandra Kurla Mumbai',
+                              },
+                              title: 'Trident Bandra Kurla',
+                              subtitle:
+                                'Bandra Kurla Complex, Mumbai | 8 kms from city center',
+                              benefits: [
+                                'Free cancellation',
+                                'Fitness center',
+                                'Business facilities',
+                              ],
+                              price: '27,999',
+                              ratings: '8.9',
+                              city: 'mumbai',
+                            },
+                            {
+                              hotelCode: 81334,
+                              image: {
+                                imageUrl:
+                                  '/images/hotels/324668211/324668211.jpg',
+                                accessibleText: 'Four Seasons Mumbai',
+                              },
+                              title: 'Four Seasons Hotel Mumbai',
+                              subtitle:
+                                'Worli, Mumbai | 4.1 kms from city center',
+                              benefits: [
+                                'Rooftop bar',
+                                'Infinity pool',
+                                'Spa and wellness center',
+                              ],
+                              price: '28,500',
+                              ratings: '8.7',
+                              city: 'mumbai',
+                            },
+                            {
+                              hotelCode: 81335,
+                              image: {
+                                imageUrl:
+                                  '/images/hotels/678321044/678321044.jpg',
+                                accessibleText: 'St. Regis Mumbai',
+                              },
+                              title: 'The St. Regis Mumbai',
+                              subtitle:
+                                'Lower Parel, Mumbai | 3.8 kms from city center',
+                              benefits: [
+                                'Butler service',
+                                'Exclusive club lounge',
+                                'Luxury spa',
+                              ],
+                              price: '33,200',
+                              ratings: '9.1',
+                              city: 'mumbai',
+                            },
+                          ],
+                        },
+                      },
+                    },
+                  },
+                ],
+              },
+            }
+          );
+        }
       });
     },
   });
