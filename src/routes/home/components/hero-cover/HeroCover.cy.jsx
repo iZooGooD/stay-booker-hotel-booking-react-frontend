39ff4d9ef3ccd40bc80a70fbbeb81cb697d7e5c0
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-01-27 11:47:17 +0530

    Remove test

--- src/routes/home/components/hero-cover/HeroCover.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import HeroCover from './HeroCover';
-
-describe('<HeroCover />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<HeroCover />);
-  });
-});
