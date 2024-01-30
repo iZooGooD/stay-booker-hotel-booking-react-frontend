c0987f7dff96b1d7027c62d2d1ad95f52205b2a6
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-01-30 08:22:09 +0530

    doc: add doc to verticalFilters component

--- src/components/vertical-filters/VerticalFilters.jsx
+++ src/components/vertical-filters/VerticalFilters.jsx
@@ -1,4 +1,15 @@
 import SbCheckbox from '../sb-checkbox/sb-checkbox';
+
+/**
+ * VerticalFilters Component
+ * Renders a vertical filter UI for filtering hotel results.
+ *
+ * @param {Object} props - Props for the component.
+ * @param {Array} props.filtersData - An array of filters data objects to display.
+ * @param {Function} props.onFiltersUpdate - Callback function to handle filter updates.
+ * @param {Function} props.onClearFiltersAction - Callback function to handle clearing of filters.
+ * @param {boolean} props.isVerticalFiltersOpen - Flag to control the visibility of the vertical filters.
+ */
 const VerticalFilters = (props) => {
   const {
     filtersData,
