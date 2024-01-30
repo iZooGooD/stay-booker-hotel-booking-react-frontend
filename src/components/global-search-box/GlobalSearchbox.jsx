c0c6b90a18bf1077a40872ae5ce2bb85272c8b34
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-01-30 08:26:00 +0530

    doc: add doc GlobalSearchBox component

--- src/components/global-search-box/GlobalSearchbox.jsx
+++ src/components/global-search-box/GlobalSearchbox.jsx
@@ -2,6 +2,24 @@ import { faLocationDot, faPerson } from '@fortawesome/free-solid-svg-icons';
 import SbInput from '../SB-Input/sb-input';
 import SbDateRangePicker from '../sb-data-range-picker/sb-date-range-picker';
 
+/**
+ * GlobalSearchBox Component
+ * Renders a search box with input fields for location, number of guests, and a date range picker.
+ * It includes a search button to trigger the search based on the entered criteria.
+ *
+ * @param {Object} props - Props for the component.
+ * @param {string} props.locationInputValue - The current value of the location input.
+ * @param {string} props.numGuestsInputValue - The current value of the number of guests input.
+ * @param {boolean} props.isDatePickerVisible - Flag to control the visibility of the date picker.
+ * @param {Function} props.onLocationChangeInput - Callback for location input changes.
+ * @param {Function} props.onNumGuestsInputChange - Callback for number of guests input changes.
+ * @param {Function} props.onDatePickerIconClick - Callback for the date picker icon click event.
+ * @param {Array} props.locationTypeheadResults - Results for the location input typeahead.
+ * @param {Function} props.onSearchButtonAction - Callback for the search button click event.
+ * @param {Function} props.onDateChangeHandler - Callback for handling date range changes.
+ * @param {Function} props.setisDatePickerVisible - Callback to set the visibility state of the date picker.
+ * @param {Object} props.dateRange - The selected date range.
+ */
 const GlobalSearchBox = (props) => {
   const {
     locationInputValue,
