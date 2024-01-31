a664cd24d210510883230215a095b521bf588d2a
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-01-31 08:22:17 +0530

    lint: run prettier

--- src/routes/about-us/AboutUs.jsx
+++ src/routes/about-us/AboutUs.jsx
@@ -8,48 +8,56 @@ const AboutUs = () => {
       <div className="text-center mt-10">
         <h1 className="text-3xl font-extrabold text-brand">About Us</h1>
         <p>
-          Welcome to STAY BOOKER, where we are dedicated to providing you with the best
-          experience for booking hotels around the world. Our mission is to make your travel
-          comfortable, convenient, and memorable.
+          Welcome to STAY BOOKER, where we are dedicated to providing you with
+          the best experience for booking hotels around the world. Our mission
+          is to make your travel comfortable, convenient, and memorable.
         </p>
         <br />
         <br />
         <h2 className="text-3xl font-extrabold text-brand">Our Vision</h2>
         <p>
-          At STAY BOOKER, we envision a world where every traveler finds the perfect
-          accommodation that suits their needs and preferences. We aim to simplify the hotel booking
-          process, offering a wide range of options for every budget.
+          At STAY BOOKER, we envision a world where every traveler finds the
+          perfect accommodation that suits their needs and preferences. We aim
+          to simplify the hotel booking process, offering a wide range of
+          options for every budget.
         </p>
         <br />
         <br />
         <h2 className="text-3xl font-extrabold text-brand">Why Choose Us?</h2>
         <p>
-          We offer a diverse range of hotels, from luxury resorts to cozy boutique
-          stays, ensuring that you find the perfect match for your travel style.
+          We offer a diverse range of hotels, from luxury resorts to cozy
+          boutique stays, ensuring that you find the perfect match for your
+          travel style.
         </p>
         <p>
           Our user-friendly interface makes it simple and quick to book your
-          ideal stay. With just a few clicks, you can secure your reservation hassle-free.
+          ideal stay. With just a few clicks, you can secure your reservation
+          hassle-free.
         </p>
         <p>
           Our dedicated customer support team is available 24/7 to assist you
-          with any inquiries or issues you may encounter during your booking process or stay.
+          with any inquiries or issues you may encounter during your booking
+          process or stay.
         </p>
         <p>
           We prioritize the security of your personal information and
-          transactions. Book with confidence, knowing that your data is safe with us.
+          transactions. Book with confidence, knowing that your data is safe
+          with us.
         </p>
         <br />
         <br />
         <h2 className="text-3xl font-extrabold text-brand">Contact Us</h2>
         <p>
-          Have questions or need assistance? Feel free to reach out to our customer support team at{' '}
-          <a className="text-brand" href="mailto:info@staybooker.com">info@staybooker.com</a>. We're here
-          to help!
+          Have questions or need assistance? Feel free to reach out to our
+          customer support team at{' '}
+          <a className="text-brand" href="mailto:info@staybooker.com">
+            info@staybooker.com
+          </a>
+          . We're here to help!
         </p>
         <p>
-          Thank you for choosing STAY BOOKER. We look forward to being your go-to
-          platform for all your hotel booking needs.
+          Thank you for choosing STAY BOOKER. We look forward to being your
+          go-to platform for all your hotel booking needs.
         </p>
         <br />
       </div>
