f2fc61336fcda597c74ef00c22b541f6d2ec7a58
johncegom <48401811+johncegom@users.noreply.github.com>
2024-02-10 16:36:58 +0700

    Issue 43: Add Logout feature (#50)

--- src/components/navbar-items/NavbarItems.jsx
+++ src/components/navbar-items/NavbarItems.jsx
@@ -1,12 +1,26 @@
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import DropdownButton from 'components/dropdown-button/DropdownButton';
+import { networkAdapter } from 'services/NetworkAdapter';
+import { useContext } from 'react';
+import { AuthContext } from 'contexts/AuthContext';
 
 const NavbarItems = ({ isAuthenticated }) => {
   const navigate = useNavigate();
   const location = useLocation();
+  const context = useContext(AuthContext);
+
+  const handleLogout = async () => {
+    const response = await networkAdapter.post('/api/logout');
+    const expectedResult = 'User logged out';
+    if (response && response.data.status === expectedResult) {
+      context.triggerAuthCheck();
+      navigate('/login');
+    }
+  };
+
   const dropdownOptions = [
     { name: 'Profile', onClick: () => navigate('/user-profile') },
-    { name: 'Logout' },
+    { name: 'Logout', onClick: handleLogout },
   ];
 
   const isActive = (path) => {
