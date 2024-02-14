55842786048f30e36a33bc1fe29982026e63f8ee
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-02-14 08:29:31 +0530

    doc: add doc to outsideClickHandler hook

--- src/hooks/useOutsideClickHandler.js
+++ src/hooks/useOutsideClickHandler.js
@@ -1,5 +1,11 @@
 import { useEffect } from 'react';
 
+/**
+ * A custom hook that triggers a callback function when a click event occurs outside of a specified element.
+ *
+ * @param {React.RefObject} ref - A React ref object attached to the element to monitor for outside clicks.
+ * @param {Function} onOutsideClick - A callback function to execute when an outside click is detected.
+ */
 const useOutsideClickHandler = (ref, onOutsideClick) => {
   useEffect(() => {
     const handleClickOutside = (event) => {
