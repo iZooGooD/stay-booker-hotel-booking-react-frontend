b70e29142cbf1fb21dbb0d582bca5c1a2ac8c15b
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-02-19 12:43:10 +0530

    fix: hide pagination for empty data for reviews

--- src/routes/hotel-details/components/user-reviews/UserReviews.jsx
+++ src/routes/hotel-details/components/user-reviews/UserReviews.jsx
@@ -76,81 +76,83 @@ const UserReviews = ({
           </div>
         )}
       </div>
-      <div className="pagination flex justify-center border-t">
-        <nav className="mt-2">
-          <ul className="flex items-center -space-x-px h-8 text-sm">
-            <li>
-              <button
-                disabled={isPreviousDisabled ? true : false}
-                onClick={handlePreviousPageChange}
-                className={`${
-                  isPreviousDisabled && 'cursor-not-allowed'
-                } flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700`}
-              >
-                <span className="sr-only">Previous</span>
-                <svg
-                  className="w-2.5 h-2.5 rtl:rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 6 10"
+      {reviewData.data.length > 0 && (
+        <div className="pagination flex justify-center border-t">
+          <nav className="mt-2">
+            <ul className="flex items-center -space-x-px h-8 text-sm">
+              <li>
+                <button
+                  disabled={isPreviousDisabled ? true : false}
+                  onClick={handlePreviousPageChange}
+                  className={`${
+                    isPreviousDisabled && 'cursor-not-allowed'
+                  } flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700`}
                 >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 1 1 5l4 4"
-                  />
-                </svg>
-              </button>
-            </li>
-            {Array.from(
-              { length: reviewData.pagination.totalPages },
-              (_, i) => (
-                <li key={i}>
-                  <button
-                    onClick={() => handlePageChange(i + 1)}
-                    className={`${
-                      parseInt(reviewData.pagination.currentPage) === i + 1
-                        ? 'bg-brand text-white'
-                        : 'bg-white text-gray-500'
-                    }  flex items-center justify-center px-3 h-8 leading-tight  border border-gray-300 hover:bg-gray-100 hover:text-gray-700`}
+                  <span className="sr-only">Previous</span>
+                  <svg
+                    className="w-2.5 h-2.5 rtl:rotate-180"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 6 10"
                   >
-                    {i + 1}
-                  </button>
-                </li>
-              )
-            )}
-            <li>
-              <button
-                disabled={isNextDisabled ? true : false}
-                onClick={handleNextPageChange}
-                className={`${
-                  isNextDisabled && 'cursor-not-allowed'
-                } flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700`}
-              >
-                <span className="sr-only">Next</span>
-                <svg
-                  className="w-2.5 h-2.5 rtl:rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 6 10"
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 1 1 5l4 4"
+                    />
+                  </svg>
+                </button>
+              </li>
+              {Array.from(
+                { length: reviewData.pagination.totalPages },
+                (_, i) => (
+                  <li key={i}>
+                    <button
+                      onClick={() => handlePageChange(i + 1)}
+                      className={`${
+                        parseInt(reviewData.pagination.currentPage) === i + 1
+                          ? 'bg-brand text-white'
+                          : 'bg-white text-gray-500'
+                      }  flex items-center justify-center px-3 h-8 leading-tight  border border-gray-300 hover:bg-gray-100 hover:text-gray-700`}
+                    >
+                      {i + 1}
+                    </button>
+                  </li>
+                )
+              )}
+              <li>
+                <button
+                  disabled={isNextDisabled ? true : false}
+                  onClick={handleNextPageChange}
+                  className={`${
+                    isNextDisabled && 'cursor-not-allowed'
+                  } flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700`}
                 >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m1 9 4-4-4-4"
-                  />
-                </svg>
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </div>
+                  <span className="sr-only">Next</span>
+                  <svg
+                    className="w-2.5 h-2.5 rtl:rotate-180"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 6 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 9 4-4-4-4"
+                    />
+                  </svg>
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
     </div>
   );
 };
