37aacad913874c5c959d688b9c09fe747f34c26d
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-02-21 08:02:03 +0530

    ci: fix failing tests

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,11 +4,6 @@
 export const MAX_GUESTS_INPUT_VALUE = 10;
 
 /**
- * Maximum number of hotels displayed on a page.
- */
-export const MAX_RESULTS_PER_PAGE = 10;
-
-/**
  * Messages related to user registration.
  */
 export const REGISTRATION_MESSAGES = {
