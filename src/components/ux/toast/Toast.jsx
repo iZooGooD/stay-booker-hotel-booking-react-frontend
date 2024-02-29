20aac7e8cb887302821958fb81e5b194b36e6758
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-02-29 09:05:51 +0530

    feat: add warning toast type

--- src/components/ux/toast/Toast.jsx
+++ src/components/ux/toast/Toast.jsx
@@ -1,10 +1,20 @@
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+/**
+ * Toast Component
+ * Displays a toast message with a dismiss button.
+ *
+ * @param {Object} props - Props for the component.
+ * @param {string} props.type - The type of toast message.
+ * @param {string} props.message - The message to display in the toast.
+ * @param {Function} props.dismissError - The function to dismiss the toast message.
+ */
 const Toast = ({ type, message, dismissError }) => {
   const typeToClassMap = {
     error: 'bg-red-100 border-l-4 border-red-500 text-red-700',
     success: 'bg-green-100 border-l-4 border-green-500 text-green-700 my-2',
+    warning: 'bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700',
   };
   return (
     <div
