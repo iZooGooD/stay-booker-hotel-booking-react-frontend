6c67823fff651d0777ed788771112eb804340a6c
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-03-01 08:40:01 +0530

    doc: add doc to image card component

--- src/routes/home/components/image-card/image-card.jsx
+++ src/routes/home/components/image-card/image-card.jsx
@@ -1,3 +1,14 @@
+import React from 'react';
+
+/**
+ * ImageCard
+ * Renders an image card with a name and image.
+ * @param {Object} props - The component props.
+ * @param {String} props.name - The name of the destination.
+ * @param {String} props.imageUrl - The image url of the destination.
+ * @param {Function} props.onPopularDestincationCardClick - The click handler for the card.
+ * @returns {JSX.Element} - The ImageCard component.
+ */
 const ImageCard = (props) => {
   const { name, imageUrl, onPopularDestincationCardClick } = props;
   return (
