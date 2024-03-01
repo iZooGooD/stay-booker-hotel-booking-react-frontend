2946a8373083df0b26f596760f7362bfc9cdd3cf
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-03-01 08:40:50 +0530

    doc: add doc to base layout

--- src/routes/layouts/base-layout/BaseLayout.jsx
+++ src/routes/layouts/base-layout/BaseLayout.jsx
@@ -3,7 +3,10 @@ import GlobalNavbar from 'components/global-navbar/GlobalNavbar';
 import { Outlet } from 'react-router-dom';
 
 /**
- * Base layout
+ * BaseLayout Component
+ * Renders the base layout for the application.
+ * It includes the global navbar, the main content, and the global footer.
+ * @returns {JSX.Element} - The BaseLayout component.
  */
 const BaseLayout = () => {
   return (
