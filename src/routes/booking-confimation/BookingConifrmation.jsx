368a61f84d98b894fb17f868345c8a0c6c9e92a4
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-03-02 17:08:15 +0530

    update: improve styling for booking confirmation card

--- src/routes/booking-confimation/BookingConifrmation.jsx
+++ src/routes/booking-confimation/BookingConifrmation.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
 const BookingConfirmation = () => {
   const bookingDetails = [
     {
@@ -26,7 +29,14 @@ const BookingConfirmation = () => {
     },
   ];
   return (
-    <div className="flex mx-auto p-4 items-center justify-center flex-col my-40">
+    <div className="flex mx-auto px-4 py-12 items-center justify-center flex-col my-40 border rounded-md max-w-[800px]">
+      <div className="flex items-center justify-center">
+        <FontAwesomeIcon icon={faStar} className="text-gray-400 text-xl" />
+        <FontAwesomeIcon icon={faStar} className="text-gray-400 text-3xl" />
+        <FontAwesomeIcon icon={faStar} className="text-gray-400 text-4xl" />
+        <FontAwesomeIcon icon={faStar} className="text-gray-400 text-3xl" />
+        <FontAwesomeIcon icon={faStar} className="text-gray-400 text-xl" />
+      </div>
       <h1 className="text-gray-700 text-2xl font-bold">Booking Confirmed</h1>
       <p className="text-gray-600 mt-2">
         Thank you for your booking! Your reservation has been confirmed.
@@ -35,7 +45,7 @@ const BookingConfirmation = () => {
         Please check your email for the booking details and instructions for
         your stay.
       </p>
-      <div className="mt-4 flex">
+      <div className="mt-4 flex justify-center flex-wrap items-center">
         {bookingDetails &&
           bookingDetails.map((detail, index) => (
             <div key={index} className="border-r-2 px-4">
