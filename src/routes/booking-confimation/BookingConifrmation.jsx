ea3644314ef3dbba6a64684cb59ded015bafba28
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-03-03 09:12:17 +0530

    feat: add navigation and print button for booking confirmation page

--- src/routes/booking-confimation/BookingConifrmation.jsx
+++ src/routes/booking-confimation/BookingConifrmation.jsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 /**
  * Represents the booking confirmation component.
@@ -25,32 +26,40 @@ const BookingConfirmation = () => {
   }, [bookingDetails, location.state, navigate]);
 
   return (
-    <div className="flex mx-2 md:mx-auto px-4 py-12 items-center justify-center flex-col my-40 border rounded-md max-w-[800px]">
-      <div className="flex items-center justify-center mb-2">
-        <FontAwesomeIcon icon={faStar} className="text-gray-400 text-xl" />
-        <FontAwesomeIcon icon={faStar} className="text-gray-400 text-3xl" />
-        <FontAwesomeIcon icon={faStar} className="text-gray-400 text-4xl" />
-        <FontAwesomeIcon icon={faStar} className="text-gray-400 text-3xl" />
-        <FontAwesomeIcon icon={faStar} className="text-gray-400 text-xl" />
+    <div className="md:mx-auto max-w-[800px] my-40">
+      <div className="flex justify-between mx-2 rounded-md my-2">
+        <Link to="/" className={`border p-2 min-w-[120px]`}>
+          Back to home
+        </Link>
+        <button className="border p-2 min-w-[120px]">Print</button>
       </div>
-      <h1 className="text-gray-700 text-2xl font-bold">Booking Confirmed</h1>
-      <p className="text-gray-600 mt-2">
-        Thank you for your booking! Your reservation has been confirmed.
-      </p>
-      <p className="text-gray-600">
-        Please check your email for the booking details and instructions for
-        your stay.
-      </p>
-      <div className="mt-4 flex justify-center flex-wrap items-center">
-        {bookingDetails &&
-          bookingDetails.map((detail, index) => (
-            <div key={index} className="border-r-2 px-4">
-              <p className="text-gray-600 text-sm">{detail.label}</p>
-              <span className="text-gray-600 text-sm font-bold">
-                {detail.value}
-              </span>
-            </div>
-          ))}
+      <div className="flex mx-2  px-4 py-12 items-center justify-center flex-col border rounded-md">
+        <div className="flex items-center justify-center mb-2">
+          <FontAwesomeIcon icon={faStar} className="text-gray-400 text-xl" />
+          <FontAwesomeIcon icon={faStar} className="text-gray-400 text-3xl" />
+          <FontAwesomeIcon icon={faStar} className="text-gray-400 text-4xl" />
+          <FontAwesomeIcon icon={faStar} className="text-gray-400 text-3xl" />
+          <FontAwesomeIcon icon={faStar} className="text-gray-400 text-xl" />
+        </div>
+        <h1 className="text-gray-700 text-2xl font-bold">Booking Confirmed</h1>
+        <p className="text-gray-600 mt-2">
+          Thank you for your booking! Your reservation has been confirmed.
+        </p>
+        <p className="text-gray-600">
+          Please check your email for the booking details and instructions for
+          your stay.
+        </p>
+        <div className="mt-4 flex justify-center flex-wrap items-center">
+          {bookingDetails &&
+            bookingDetails.map((detail, index) => (
+              <div key={index} className="border-r-2 px-4">
+                <p className="text-gray-600 text-sm">{detail.label}</p>
+                <span className="text-gray-600 text-sm font-bold">
+                  {detail.value}
+                </span>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
