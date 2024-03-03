386418369675382f833fdfda6fa8148ed3f8d63b
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-03-03 09:26:40 +0530

    feat: add functionality for print button

--- src/routes/booking-confimation/BookingConifrmation.jsx
+++ src/routes/booking-confimation/BookingConifrmation.jsx
@@ -15,6 +15,15 @@ const BookingConfirmation = () => {
 
   const [bookingDetails, setBookingDetails] = useState(null);
 
+  /**
+   * Handles the print event.
+   * @function
+   * @returns {void}
+   */
+  const onPrintHandler = () => {
+    window.print();
+  };
+
   // Set booking details from location state passed from the previous page(checkout page)
   useEffect(() => {
     if (location.state) {
@@ -28,18 +37,26 @@ const BookingConfirmation = () => {
   return (
     <div className="md:mx-auto max-w-[800px] my-40">
       <div className="flex justify-between mx-2 rounded-md my-2">
-        <Link to="/" className={`border p-2 min-w-[120px]`}>
+        <Link
+          to="/"
+          className={`border p-2 min-w-[120px] transition-all delay-100 hover:bg-brand hover:text-white`}
+        >
           Back to home
         </Link>
-        <button className="border p-2 min-w-[120px]">Print</button>
+        <button
+          onClick={onPrintHandler}
+          className="border p-2 min-w-[120px] transition-all delay-75 hover:bg-gray-500 hover:text-white"
+        >
+          Print
+        </button>
       </div>
       <div className="flex mx-2  px-4 py-12 items-center justify-center flex-col border rounded-md">
         <div className="flex items-center justify-center mb-2">
-          <FontAwesomeIcon icon={faStar} className="text-gray-400 text-xl" />
-          <FontAwesomeIcon icon={faStar} className="text-gray-400 text-3xl" />
-          <FontAwesomeIcon icon={faStar} className="text-gray-400 text-4xl" />
-          <FontAwesomeIcon icon={faStar} className="text-gray-400 text-3xl" />
-          <FontAwesomeIcon icon={faStar} className="text-gray-400 text-xl" />
+          <FontAwesomeIcon icon={faStar} className="text-brand text-xl" />
+          <FontAwesomeIcon icon={faStar} className="text-brand text-3xl" />
+          <FontAwesomeIcon icon={faStar} className="text-brand text-4xl" />
+          <FontAwesomeIcon icon={faStar} className="text-brand text-3xl" />
+          <FontAwesomeIcon icon={faStar} className="text-brand text-xl" />
         </div>
         <h1 className="text-gray-700 text-2xl font-bold">Booking Confirmed</h1>
         <p className="text-gray-600 mt-2">
