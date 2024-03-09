25e4c7fef86daa620de5406bd93f6afaf295b883
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-03-09 09:24:56 +0530

    doc: add doc to scroll-to-top

--- src/components/scroll-to-top/ScrollToTop.js
+++ src/components/scroll-to-top/ScrollToTop.js
@@ -1,6 +1,11 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+/**
+ * A component that scrolls to the top of the page when the route changes.
+ * @component
+ * @returns {null}
+ */
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
