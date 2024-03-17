c6aa990058928b1737a667190484e1bf2faced08
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-03-17 14:03:33 +0530

    chore: move common validation to parent class

--- src/utils/validation-schemas.js
+++ src/utils/validation-schemas.js
@@ -1,7 +1,11 @@
 import * as Yup from 'yup';
 const phoneRegExp = /^\d{10}$/;
 
-class Schemas {
+class ValidationSchema {
+  static email = Yup.string().email('Invalid email').required('Required');
+}
+
+class Schemas extends ValidationSchema {
   static signupSchema = Yup.object().shape({
     firstName: Yup.string()
       .min(2, 'Too Short!')
@@ -11,7 +15,7 @@ class Schemas {
       .min(2, 'Too Short!')
       .max(50, 'Too Long!')
       .required('Required'),
-    email: Yup.string().email('Invalid email').required('Required'),
+    email: ValidationSchema.email,
     phoneNumber: Yup.string()
       .matches(phoneRegExp, 'Phone number is not valid')
       .required('Required'),
